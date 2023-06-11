a39583f5ad8cd3a84338d70fa80b9a3fb1be2ee8
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-06-11 10:30:38 +0300

    updated charts and responsiveness

--- js/index.js
+++ js/index.js
@@ -1,125 +1,145 @@
+// SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  if(!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if(sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
+
+
+
+// ---------- CHARTS ----------
+
+
 
 // BAR CHART
 var barChartOptions = {
-    series: [{
-      data: [10, 8, 6, 4, 2]
-    }],
-    chart: {
-      type: 'bar',
-      height: 350,
-      toolbar: {
-        show: false
-      },
-    },
-    colors: [
-      "#2CB629"
-     
-    ],
-    plotOptions: {
-      bar: {
-        distributed: true,
-        borderRadius: 4,
-        horizontal: false,
-        columnWidth: '40%',
-      }
-    },
-    dataLabels: {
-      enabled: false
-    },
-    legend: {
+  series: [{
+    data: [10, 8, 6, 4, 2]
+  }],
+  chart: {
+    type: 'bar',
+    height: 350,
+    toolbar: {
       show: false
     },
-    xaxis: {
-      categories: ["Tax", "Rent","wifi","Phone", "Monitor", "Headphones", "Camera"],
-    },
-    yaxis: {
-      title: {
-        text: "Count"
-      }
+  },
+  colors: [
+    "#2CB629"
+   
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: '40%',
     }
-  };
-  
-  var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
-  barChart.render();
+  },
+  dataLabels: {
+    enabled: false
+  },
+  legend: {
+    show: false
+  },
+  xaxis: {
+    categories: ["Tax", "Rent","wifi","Phone", "Monitor", "Headphones", "Camera"],
+  },
+  yaxis: {
+    title: {
+      text: "Count"
+    }
+  }
+};
 
-
-
-
-  // line CHART
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
+// line CHART
 var areaChartOptions = {
-    series: [{
-      name: 'Purchase Orders',
-      data: [31, 40, 28, 51, 42, 109, 100]
-    }, {
-      name: 'Sales Orders',
-      data: [11, 32, 45, 32, 34, 52, 41]
-    }],
-    chart: {
-      height:500,
-      type: 'line',
-      toolbar: {
-        show: false,
+  series: [{
+    name: 'Purchase Orders',
+    data: [31, 40, 28, 51, 42, 109, 100]
+  }, {
+    name: 'Sales Orders',
+    data: [11, 32, 45, 32, 34, 52, 41]
+  }],
+  chart: {
+    height:500,
+    type: 'line',
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#4f35a1", "#246dec"],
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+  markers: {
+    size: 0
+  },
+  yaxis: [
+    {
+      title: {
+        text: 'Purchase Orders',
       },
     },
-    colors: ["#4f35a1", "#246dec"],
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: 'smooth'
-    },
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-    markers: {
-      size: 0
-    },
-    yaxis: [
-      {
-        title: {
-          text: 'Purchase Orders',
-        },
+    {
+      opposite: true,
+      title: {
+        text: 'Sales Orders',
       },
-      {
-        opposite: true,
-        title: {
-          text: 'Sales Orders',
-        },
-      },
-    ],
-    tooltip: {
-      shared: true,
-      intersect: false,
-    }
-  };
-  
-  var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
-  areaChart.render();
-  // donut chart
-  const chartData = {
-    series: [44, 55, 13],
-    labels: ['Expenses', 'Income', 'Savings']
-  };
+    },
+  ],
+  tooltip: {
+    shared: true,
+    intersect: false,
+  }
+};
 
-  // Options for the chart
-  const chartOptions = {
-    chart: {
-      type: 'donut'
-    },
-    series: chartData.series,
-    labels: chartData.labels,
-    responsive: [{
-      breakpoint: 480,
-      options: {
-        chart: {
-          width:200,
-        
-        },
-        legend: {
-          position: 'bottom'
-        }
+var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+areaChart.render();
+// donut chart
+const chartData = {
+  series: [44, 55, 13],
+  labels: ['Expenses', 'Income', 'Savings']
+};
+
+// Options for the chart
+const chartOptions = {
+  chart: {
+    type: 'donut'
+  },
+  series: chartData.series,
+  labels: chartData.labels,
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width:200,
+      
+      },
+      legend: {
+        position: 'bottom'
       }
-    }]
-  };
+    }
+  }]
+};
 
-  // Create the chart
-  const chart = new ApexCharts(document.querySelector("#donut-chart"), chartOptions);
-  chart.render();
+// Create the chart
+const chart = new ApexCharts(document.querySelector("#donut-chart"), chartOptions);
+chart.render();
